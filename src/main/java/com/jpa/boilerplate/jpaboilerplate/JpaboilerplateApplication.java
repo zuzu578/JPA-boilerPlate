c200a68b8438bc693930a54e9c219d3739336985
zuzu <dlwnghks6821@naver.com>
2022-05-23 12:42:47 +0900

    # test

--- src/main/java/com/jpa/boilerplate/jpaboilerplate/JpaboilerplateApplication.java
+++ src/main/java/com/jpa/boilerplate/jpaboilerplate/JpaboilerplateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = { "com.jpa.boilerplate.jpaboilerplate.repository" })
-@EntityScan(basePackages = { "com.jpa.boilerplate.jpaboilerplate.entity" })
+// @EnableJpaRepositories(basePackages = { "com.jpa.boilerplate.jpaboilerplate.repository" })
+// @EntityScan(basePackages = { "com.jpa.boilerplate.jpaboilerplate.entity" })
 public class JpaboilerplateApplication {
 
 	public static void main(String[] args) {
