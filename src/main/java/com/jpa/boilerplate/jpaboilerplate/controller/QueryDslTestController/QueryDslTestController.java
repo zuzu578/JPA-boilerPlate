7d59038c5b0b2842fdf2e8fcefe3e5058f54ff49
zuzu <dlwnghks6821@naver.com>
2022-06-16 11:28:13 +0900

    # query dsl 방식으로 test api 생성

--- src/main/java/com/jpa/boilerplate/jpaboilerplate/controller/QueryDslTestController/QueryDslTestController.java
+++ src/main/java/com/jpa/boilerplate/jpaboilerplate/controller/QueryDslTestController/QueryDslTestController.java
@@ -1,35 +1,37 @@
 package com.jpa.boilerplate.jpaboilerplate.controller.QueryDslTestController;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jpa.boilerplate.jpaboilerplate.entity.Board.BoardEntity;
+import com.jpa.boilerplate.jpaboilerplate.entity.Board.QBoardEntity;
 import com.mysema.query.jpa.impl.JPAQuery;
 
 @RestController
+@RequestMapping("/test")
 public class QueryDslTestController {
 
     @PersistenceContext
     EntityManager entityManager;
 
     @GetMapping("/test")
-    public void test() {
+    public ResponseEntity<?> test() {
         JPAQuery query = new JPAQuery(entityManager);
-        // QBoardEntity test = new QBoardEntity("m");
-        // JPAQuery query = new JPAQuery(entityManager);
-        // QBoardEntity qMember = new QBoardEntity("m"); // 생성되는 JPQL의 별칭이 m
-        // List<Member> members = query
-        // .from(qMember)
-        // .where(qMember.username.eq("철수"))
-        // .orderBy(qMember.username.desc())
-        // .list(qMember);
+        QBoardEntity qBoard = new QBoardEntity("m"); // 생성되는 JPQL의 별칭이 m
+        List<BoardEntity> result = query
+                .from(qBoard)
+                .where(qBoard.title.like("수정함"))
+                .orderBy(qBoard.createdTime.desc())
+                .list(qBoard);
 
-        // for (Member member : members) {
-        // System.out.println("Member : " + member.getMemberId() + ", " +
-        // member.getUsername());
-        // }
-        // }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
