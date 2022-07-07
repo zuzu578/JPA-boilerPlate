ddc00e702221dee21a285e5de56a7ab625093238
zuzu <dlwnghks6821@naver.com>
2022-07-07 22:18:55 +0900

    # criteria inner / left test

--- src/main/java/com/jpa/boilerplate/jpaboilerplate/controller/CriteriaSampleController/CriteriaSample.java
+++ src/main/java/com/jpa/boilerplate/jpaboilerplate/controller/CriteriaSampleController/CriteriaSample.java
@@ -1,6 +1,7 @@
 package com.jpa.boilerplate.jpaboilerplate.controller.CriteriaSampleController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -28,29 +29,45 @@ public class CriteriaSample {
      * sample code
      */
     @GetMapping("/criteriaTest")
-    public void test() {
+    public List<HashMap<String, Object>> test() {
         List<Tuple> tuples = new ArrayList<Tuple>();
+
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        HashMap<String, Object> resultMap = null;
+        List<HashMap<String, Object>> resultArr = new ArrayList<HashMap<String, Object>>();
 
         CriteriaQuery<Tuple> criteria = criteriaBuilder.createTupleQuery();
         Root<BoardEntity> board = criteria.from(BoardEntity.class);
-        Join<BoardEntity, BoardCommentEntity> boardComment = board.join("boardComment", JoinType.LEFT);
+        Join<BoardEntity, BoardCommentEntity> boardComment = board.join("boardComment", JoinType.INNER);
 
         try {
             // where clause
-            Predicate conditions = criteriaBuilder.equal(board.get("name"), "test");
-            criteria.where(conditions);
-            criteria.multiselect(board, boardComment);
-            tuples = entityManager.createQuery(criteria.where()).getResultList();
+            Predicate conditions = criteriaBuilder.equal(boardComment.get("commentNo"), 4);
+            criteria.multiselect(board, boardComment).where(conditions);
+            tuples = entityManager.createQuery(criteria).getResultList();
             // todo : tuple 에서 목록 꺼내서 ListHashMap으로 변환 + 조건에 맞게 filter
             for (Tuple tuple : tuples) {
-                System.out.println("test ===>" + tuple.get(board).getTitle());
+                resultMap = new HashMap<String, Object>();
+                resultMap.put("title", tuple.get(board).getTitle());
+                resultMap.put("boardNo", tuple.get(board).getBoardNo());
+                resultMap.put("contents", tuple.get(board).getContent());
+                resultMap.put("name", tuple.get(board).getName());
+                resultMap.put("createdTime", tuple.get(board).getCreatedTime());
+
+                resultMap.put("comment", tuple.get(boardComment).getComment());
+
+                resultArr.add(resultMap);
 
             }
 
+            return resultArr;
+
         } catch (Exception e) {
             e.printStackTrace();
+            // test
+            return null;
         }
+
     }
 
 }
